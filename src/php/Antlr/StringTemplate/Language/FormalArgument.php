45e638d9ee8f7fd6b334f4245bd38d7f80b35c01
Yauhen Yakimovich <ewger@ewger-laptop.(none)>
2010-09-18 11:33:08 +0200

    FormalArgument was translated from Java

--- src/php/Antlr/StringTemplate/Language/FormalArgument.php
+++ src/php/Antlr/StringTemplate/Language/FormalArgument.php
@@ -1,11 +1,42 @@
-package org.antlr.stringtemplate.language;
+<?php
+/*
+ * [The "BSD licence"]
+ * Copyright (c) 2003-2005 Terence Parr, 2010 Yauhen Yakimovich
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Antlr
+ * @package   Antlr_StringTemplate
+ * @author    Terence Parr
+ * @author    Yauhen Yakimovich
+ * @copyright 2003-2005 Terence Parr, 2010 Yauhen Yakimovich
+ * @license   http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * @link      http://www.stringtemplate.org/
+*/
+namespace Antlr\StringTemplate\Language;
 
-import org.antlr.stringtemplate.StringTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.LinkedHashMap;
+use Antlr\StringTemplate\StringTemplate;
 
 /** Represents the name of a formal argument
  *  defined in a template:
@@ -21,15 +52,23 @@ import java.util.LinkedHashMap;
  *  Note: originally, I tracked cardinality as well as the name of an
  *  attribute.  I'm leaving the code here as I suspect something may come
  *  of it later.  Currently, though, cardinality is not used.
+ *
+ * @category  Antlr
+ * @package   Antlr_StringTemplate
+ * @author    Terence Parr
+ * @author    Yauhen Yakimovich
+ * @copyright 2003-2005 Terence Parr, 2010 Yauhen Yakimovich
+ * @license   http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * @link      http://www.stringtemplate.org/
  */
-public class FormalArgument {
+class FormalArgument {
     // the following represent bit positions emulating a cardinality bitset.
-    public static final int OPTIONAL = 1;     // a?
-    public static final int REQUIRED = 2;     // a
-    public static final int ZERO_OR_MORE = 4; // a*
-    public static final int ONE_OR_MORE = 8;  // a+
+    const OPTIONAL = 1;     // a?
+    const REQUIRED = 2;     // a
+    const ZERO_OR_MORE = 4; // a*
+    const ONE_OR_MORE = 8;  // a+
 
-    public static final String[] suffixes = {
+    static public $suffixes = array(
         null,
         "?",
         "",
@@ -39,60 +78,63 @@ public class FormalArgument {
         null,
         null,
         "+"
-    };
+    );
 
     /** When template arguments are not available such as when the user
      *  uses "new StringTemplate(...)", then the list of formal arguments
      *  must be distinguished from the case where a template can specify
      *  args and there just aren't any such as the t() template above.
      */
-    public static final LinkedHashMap UNKNOWN = new LinkedHashMap();
+    //static public $UNKNOWN = array();
+    const UNKNOWN = 16;
 
-    public String name;
+    /**
+     * @var string
+     */
+    public $name;
     //protected int cardinality = REQUIRED;
 
-	/** If they specified name="value", store the template here */
-	public StringTemplate defaultValueST;
+	/** If they specified name="value", store the template here
+     * @var StringTemplate
+     */
+	public $defaultValueST;
 
-	public FormalArgument(String name) {
-		this.name = name;
+	public function FormalArgument($name, $defaultValueST = null) {
+		$this->name = $name;
+        if ($defaultValueST) {
+            $this->defaultValueST = $defaultValueST;
+        }
 	}
 
-	public FormalArgument(String name, StringTemplate defaultValueST) {
-		this.name = name;
-		this.defaultValueST = defaultValueST;
-	}
-
-    public static String getCardinalityName(int cardinality) {
+    public static function getCardinalityName($cardinality) {
         switch (cardinality) {
-            case OPTIONAL : return "optional";
-            case REQUIRED : return "exactly one";
-            case ZERO_OR_MORE : return "zero-or-more";
-            case ONE_OR_MORE : return "one-or-more";
+            case self::OPTIONAL : return "optional";
+            case self::REQUIRED : return "exactly one";
+            case self::ZERO_OR_MORE : return "zero-or-more";
+            case self::ONE_OR_MORE : return "one-or-more";
             default : return "unknown";
         }
     }
 
-	public boolean equals(Object o) {
-		if ( o==null || !(o instanceof FormalArgument) ) {
+	public function equals($object) {
+		if ( $object==null || !($object instanceof FormalArgument) ) {
 			return false;
-		}
-		FormalArgument other = (FormalArgument)o;
-		if ( !this.name.equals(other.name) ) {
+		}		
+		if ( !$this->name->equals($object->name) ) {
 			return false;
 		}
 		// only check if there is a default value; that's all
-		if ( (this.defaultValueST!=null && other.defaultValueST==null) ||
-			 (this.defaultValueST==null && other.defaultValueST!=null) ) {
+		if ( ($this->defaultValueST!=null && $object->defaultValueST==null) ||
+			 ($this->defaultValueST==null && $object->defaultValueST!=null) ) {
 			return false;
 		}
 		return true;
 	}
 
-    public String toString() {
-		if ( defaultValueST!=null ) {
-			return name+"="+defaultValueST;
+    public function toString() {
+		if ( $this->defaultValueST!=null ) {
+			return $this->name . "=" . $this->defaultValueST;
 		}
-        return name;
+        return $this->name;
     }
 }
